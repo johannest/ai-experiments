0b0b7e84595a9e33ffd7b232f28aa40ecbf210d0
Johannes Tuikkala <johannes@vaadin.com>
2017-06-22 13:04:58 +0300

    small fixes

--- src/main/java/org/vaadin/ai/api/TensorFlowController.java
+++ src/main/java/org/vaadin/ai/api/TensorFlowController.java
@@ -1,5 +1,6 @@
 package org.vaadin.ai.api;
 
+import java.util.Base64;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,31 +11,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Base64;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 
 @Service
 @RestController
 @RequestMapping("/api/tensorflow")
 public class TensorFlowController {
 
+    private static final String modelFileName = "tensorflow_inception_graph.pb";
+    private static final String labelsFileName = "imagenet_comp_graph_label_strings.txt";
+
+
     final TensorFlowProvider tf;
 
     public TensorFlowController() {
+        String tmpFolder = System.getProperty("java.io.tmpdir");
+
+        readFromClassResourcesAndWriteIntoTmpFolder(modelFileName, tmpFolder);
+        readFromClassResourcesAndWriteIntoTmpFolder(labelsFileName, tmpFolder);
+
         tf = new TensorFlowProvider(
-                getTFDirectoryPath() + "tensorflow_inception_graph.pb",
-                getTFDirectoryPath() + "imagenet_comp_graph_label_strings.txt");
+                tmpFolder +"/"+ modelFileName,
+                tmpFolder +"/"+ labelsFileName);
     }
 
-    private String getTFDirectoryPath() {
-        return TensorFlowController.class.getResource("../../../../tf/")
-                .getPath();
+    private void readFromClassResourcesAndWriteIntoTmpFolder(String filename, String targetFolder) {
+        // read as byte stream and save into tmp folder
+        InputStream in = getClass().getResourceAsStream("/tf/"+filename);
+
+        File target = new File(targetFolder+"/"+filename);
+
+        try {
+            Files.copy(in, target.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST)
